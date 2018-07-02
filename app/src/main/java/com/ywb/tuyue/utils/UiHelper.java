bc2f09ffa5674242678ea4b584b3b2bcfdb45320
wcy <wcystart0706@163.com>
2018-07-02 09:49:01 +0800

    添加gitignore文件

--- app/src/main/java/com/ywb/tuyue/utils/UiHelper.java
+++ app/src/main/java/com/ywb/tuyue/utils/UiHelper.java
@@ -2,6 +2,7 @@ package com.ywb.tuyue.utils;
 
 import android.app.Activity;
 import android.content.Context;
+
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
